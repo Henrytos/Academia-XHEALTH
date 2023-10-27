7f8ae3e11de3d35fcf0ff4d88374621a0f949d18
HENRY <franzhenry46@gmail.com>
2023-10-27 10:43:31 -0300

    fznd alteração nos textos

--- src/js/treinos.js
+++ src/js/treinos.js
@@ -2,29 +2,29 @@ const treinos = [
   [
     {
       typeTreino: "PEITO",
-      title: "Supino reto",
-      src: "https://images.unsplash.com/photo-1534368959876-26bf04f2c947?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1470",
-      subTitle: "Como executá-lo?",
-      text: "“Para realizar o supino, você pode usar pesos livres ou barra, deitado em um banco. Antes de iniciar, certifique-se de que o banco é adequado para atividades físicas  e que sua postura está correta. Se achar necessário, fale com um profissional”, aconselha Valter.",
+      title: "Supino Inclinado",
+      src: "https://images.unsplash.com/photo-1584466977773-e625c37cdd50?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1287",
+      subTitle: "Supino inclinado é uma ótima opção para o peitoral, principalmente para a parte superior a qual a maioria carece.",
+      text: "Ele deve ser feito em um banco inclinado entre 30° e 45° apoie seus pés no chão e retraia as escápulas (preferencialmente use halteres).",
     },
     {
-      title: "Supino inclinado",
-      src: "https://images.unsplash.com/photo-1584466977773-e625c37cdd50?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1287",
-      subTitle: "Como executá-lo?",
-      text: "Ajuste o banco para a posição inclinado (entre 30º e 45º graus). Mantenha os pés apoiados no chão ou no suporte do banco. Deitado com os braços estendidos (Posição inicial) inicie a execução do movimento flexionando os braços (Faça um ângulo de aproximadamente 90º graus).",
+      title: "Supino Reto",
+      src: "https://images.unsplash.com/photo-1534368959876-26bf04f2c947?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1470",
+      subTitle: "Esse exercício, feito corretamente serve para o peitoral médio.",
+      text: "Deixe o banco reto, retraia as escápulas e desça com a barra até perto do peito, mantenha os pés apoiados no chão ou no suporte do banco (preferencialmente use halteres).",
     },
     {
       title: "Supino declinado",
-      src: "https://i0.wp.com/post.healthline.com/wp-content/uploads/2019/05/Decline_Bench_Press_1296x728-header-1296x728.jpg?w=1155&h=1528",
-      subTitle: "O QUE É TREINO FUNCIONAL",
-      text: "Supino declinado. Segue o mesmo princípio do anterior, mas são as pernas que ficam voltadas para cima. Trabalha a musculatura peitoral inferior.",
+      src: "https://www.g4physio.co.uk/wp-content/uploads/2016/11/Decline-DB-Press-3-scaled.jpg",
+      subTitle: "Este exercício é o inverso do supino inclinado assim trabalhando a parte inferior do peitoral.",
+      text: "Para este exercício decline seu banco de supino discretamente, se não tiver essa opção coloque uma anilha em baixo do apoio do banco, e faça a excecução padrão do supino (preferencialmente use halteres).",
     },
   ],
   [
     {
-      typeTreino: "BRAÇO",
-      title: "TREINO FUNCIONAL",
-      src: "https://blog.gsuplementos.com.br/wp-content/uploads/2021/04/iStock-1246046696.jpg",
+      typeTreino: "TRICEPS",
+      title: "Tricep frances",
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvC3c8OXqMXZhyqztFEpSp-dbt5fy5UXokJg&usqp=CAU",
       subTitle: "O QUE É TREINO FUNCIONAL",
       text: "Esse tipo de treinamento pode ser definido como um grupo de exercícios mais dinâmicos que trabalham diferentes músculos ao mesmo tempo, sem a necessidade de aparelhos de musculação ou atividades de alto impacto, como as do crossfit.    Esse modelo de treino usa o peso do próprio corpo para a realização dos exercícios e, além do corpo físico, os exercícios funcionais também trabalham a mente e a concentração.",
     },
@@ -41,5 +41,17 @@ const treinos = [
       text: "Esse tipo de treinamento pode ser definido como um grupo de exercícios mais dinâmicos que trabalham diferentes músculos ao mesmo tempo, sem a necessidade de aparelhos de musculação ou atividades de alto impacto, como as do crossfit.    Esse modelo de treino usa o peso do próprio corpo para a realização dos exercícios e, além do corpo físico, os exercícios funcionais também trabalham a mente e a concentração.",
     },
   ],
+  [
+    {
+      typeTreino: "PEITO",
+      title: "Supino Declinado",
+      src: "https://images.unsplash.com/photo-1534368959876-26bf04f2c947?auto=format&fit=crop&q=80&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&w=1470",
+      subTitle: "Como executá-lo?",
+      text: "“Para realizar o supino, você pode usar pesos livres ou barra, deitado em um banco. Antes de iniciar, certifique-se de que o banco é adequado para atividades físicas  e que sua postura está correta. Se achar necessário, fale com um profissional”, aconselha Valter.",
+    
+    },
+    {},
+    {}
+  ]
 ];
 export default treinos;
